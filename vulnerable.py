01a681f5ac99cf4e7355f9323803dbdad48e0fa1
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-11-08 21:44:15 +0300

    updated assignment code

--- vulnerable.py
+++ vulnerable.py
@@ -7,20 +7,20 @@ def decr(ciphertext):
   return ispkcs7(cipher.decrypt(ciphertext))
 
 def ispkcs7(plaintext):
-  l = len(plaintext)
-  c = ord(plaintext[l-1])                       
-  if (c > 16) or (c < 1):
-    return "ERROR"
-  if plaintext[l-c:] != chr(c)*c:
-    return "ERROR"
-  return plaintext
+    l = len(plaintext)
+    c = ord(plaintext[l-1])                       
+    if (c > 16) or (c < 1):
+        return "ERROR"
+    if plaintext[l-c:] != chr(c)*c:
+        return "ERROR"
+    return "SUCCESS"
 
 def encr(plaintext):
-  cipher = AES.new(key, AES.MODE_CBC, iv)
-  ciphertext = cipher.encrypt(pkcs7(plaintext))
-  return ciphertext
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    ciphertext = cipher.encrypt(pkcs7(plaintext))
+    return ciphertext
 
 def pkcs7(plaintext):
-  padbytes = 16 - len(plaintext) % 16
-  pad = padbytes * chr(padbytes)
-  return plaintext + pad
+    padbytes = 16 - len(plaintext) % 16
+    pad = padbytes * chr(padbytes)
+    return plaintext + pad
