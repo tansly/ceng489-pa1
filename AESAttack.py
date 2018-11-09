fd7ef3355489a44b45aa16ac611966c2e375afcf
Yağmur Oymak <yagmur.oymak@gmail.com>
2018-11-09 23:23:13 +0300

    progress

--- AESAttack.py
+++ AESAttack.py
@@ -2,6 +2,7 @@
 
 import vulnerable
 
+import datetime
 import random
 import sys
 
@@ -14,16 +15,21 @@ def main(argv):
     #
     cipher_bytes = map(ord, ciphertext)
     cipher_mod = cipher_bytes[0:16] + [random.randint(0x80, 0xff) for i in xrange(16)] + cipher_bytes[32:48]
-    print(cipher_mod)
     decr_out = []
-    for i in xrange(31, 15, -1):
-        orig_byte = cipher_bytes[i]
-        for bval in xrange(0, 0xff):
-            cipher_mod[i] = bval
+    for n_pad in xrange(1, 16):
+        #print(vulnerable.decr_dbg(''.join(map(chr, cipher_mod))))
+        #print('--------------------------------------------')
+        for bval in xrange(0, 256):
+            cipher_mod[32 - n_pad] = bval
             if vulnerable.decr(''.join(map(chr, cipher_mod))) == "SUCCESS":
-                decr_out.append(bval^0x01^orig_byte)
-                cipher_mod[i] = decr_out[-1]^0x02
+                decr_out.insert(0, bval^n_pad)
+                cipher_mod[32 - n_pad:32] = [x^y for (x, y) in zip(decr_out, [n_pad + 1]*n_pad)]
+                #print(vulnerable.decr_dbg(''.join(map(chr, cipher_mod))))
+                #print('a')
                 print(decr_out)
+                break
+        #print(cipher_mod[16:32])
 
 if __name__ == "__main__":
+    random.seed(datetime.datetime.now())
     main(sys.argv)
